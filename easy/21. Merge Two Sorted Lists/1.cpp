e0259f63e27b5f84e08d045260897409e2e510ff
SangEon <86558575+sangeon-ahn@users.noreply.github.com>
2024-04-20 23:23:47 +0900

    21. Merge Two Sorted Lists

---
+++ easy/21. Merge Two Sorted Lists/1.cpp
@@ -0,0 +1,38 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head = new ListNode();
+
+        ListNode* temp = head;
+        while (list1 || list2) {
+            if (list1 == nullptr) {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            else if (list2 == nullptr) {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                if (list1->val >= list2->val) {
+                    temp->next = list2;
+                    list2 = list2->next;
+                }
+                else {
+                    temp->next = list1;
+                    list1 = list1->next;
+                }
+            }
+            temp = temp->next;
+        }
+        return head->next;
+    }
+};
